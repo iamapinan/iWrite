fe94e77944c5e7312fc04b2fdef3a4dd26fdc9c8
Apinan Woratrakun <iamapinan@gmail.com>
2019-02-11 18:51:39 +0700

    update latest

--- header.php
+++ header.php
@@ -20,16 +20,16 @@
 	<div class="header shadow-sm">
 	<header id="masthead" class="site-header navbar">
 		<div class="site-top container">
-			<div class="site-top-table">
-				<div class="site-branding">
-				<?php if ( function_exists( 'the_custom_logo' ) ) {
-					the_custom_logo();
-				} ?>
-				<?php write_site_title(); ?>
-				<div class="site-description"><?php bloginfo( 'description' ); ?></div>
+			<div class="site-top-table d-flex flex-row justify-content-between">
+				<div class="site-branding justify-content-start align-self-end">
+					<?php if ( function_exists( 'the_custom_logo' ) ) {
+						the_custom_logo();
+					} ?>
+					<?php write_site_title(); ?>
+					<div class="site-description"><?php bloginfo( 'description' ); ?></div>
 				</div><!-- .site-branding -->
 
-				<nav id="site-navigation" class="main-navigation">
+				<nav id="site-navigation" class="main-navigation justify-content-end align-self-end">
 					<button class="drawer-toggle drawer-hamburger">
 						<span class="screen-reader-text"><?php esc_html_e( 'Menu', 'write' ); ?></span>
 						<span class="drawer-hamburger-icon"></span>
@@ -37,15 +37,17 @@
 					<div class="drawer-nav">
 						<div class="drawer-content">
 							<div class="drawer-content-inner">
-							<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-						</div><!-- .drawer-content-inner -->
-					</div><!-- .drawer-content -->
-				</div><!-- .drawer-nav -->
-			</nav><!-- #site-navigation -->
+								<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+							</div><!-- .drawer-content-inner -->
+						</div><!-- .drawer-content -->
+					</div><!-- .drawer-nav -->
+				</nav><!-- #site-navigation -->
 			<!-- search -->
 			<?php if ( get_theme_mod( 'iwrite_show_search' ) ) : ?>
-				<?php get_search_form(); ?>
-			<?php endif; ?>
+				<div class="justify-content-end align-self-end ml-3">
+					<?php get_search_form(); ?>
+				</div>
+				<?php endif; ?>
 
 			</div><!-- .site-top-table -->
 		</div><!-- .site-top -->
