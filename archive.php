9f0fee5c8a0f52d6a63f9f2f95c60d708d9ffcb1
naruedom khunrong <naruedom@iotech.co.th>
2019-02-12 23:05:58 +0700

    update tag page (issue)

--- archive.php
+++ archive.php
@@ -14,10 +14,42 @@ get_header(); ?>
 	<?php if ( have_posts() ) : ?>
 
 		<header class="archive-header">
-			<?php
-				the_archive_title( '<h1 class="iwrite-category-title mt-5">', '</h1>' );
-				the_archive_description( '<div class="taxonomy-description">', '</div>' );
-			?>
+		
+			<div class="row border-bottom pb-5 mt-5">
+			
+				<?php if(function_exists('z_taxonomy_image')) : ?>
+				<div class="col-md-3">
+					<?php z_taxonomy_image(); ?>
+				</div>
+				<?php endif; ?>
+
+				<div class="col">
+					<div class="row">
+						<div class="col">
+						<?php 
+							the_archive_title( '<h2 class="iwrite-category-title mt-2 mt-md-0">', '</h2>' ); 
+						?>
+						</div>
+						<div class="col-md-4">
+							<div class="form-group">
+								<input class="form-control" list="issues" name="issue" placeholder="ค้าหา Issue" oninput="checkIssue(this.value)">
+								<datalist id="issues">
+									<?php $tags = get_tags();
+										foreach ( $tags as $tag ) :
+									?>
+										<option value="<?php echo $tag->slug; ?>" onclick="alert('click')"><?php echo $tag->count; ?></option>
+									<?php 
+									endforeach 
+									?>
+								</datalist>
+							</div>
+						</div>
+					</div>
+					<?php
+						the_archive_description( '<p class="taxonomy-description">sadfsdaf', '</p>' );
+					?>
+				</div>
+			</div>
 		</header><!-- .page-header -->
 
 		<div class="row mt-5">
@@ -42,5 +74,17 @@ get_header(); ?>
 
 	</main><!-- #main -->
 </section><!-- #primary -->
+
+<script>
+	var tags = <?php echo json_encode(get_tags()) ?>;
+	
+	var issues = tags.map(tag => tag.slug);
+
+	function checkIssue(value){
+		if(issues.includes(value)) {
+			window.location.replace( '<?php echo get_site_url(); ?>/tag/'+value );
+		}
+	}
+</script>
 
 <?php get_footer(); ?>
